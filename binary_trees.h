fdcb18da9efe089e127213148f5262f4e4a27d8c
“bakosh2” <bakosh2@hotmail.com>
2025-12-10 20:36:12 +0000

    write code in 0-binary_tree_node.c file and write Description of binary_tree_t Structure in README.md

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,16 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 typedef struct binary_tree_s
 {
     int n;
@@ -5,4 +18,9 @@ typedef struct binary_tree_s
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 } binary_tree_t;
+
+/* Prototype for task 0 */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif
 
